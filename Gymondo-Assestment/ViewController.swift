ecf3a512f6fb2e37d690e0e1e72416a1c30d7c54
Erfan Andesta <erfan.andesta@mindvalley.com>
2022-06-11 18:48:52 +0430

    [CORE] Init project

---
+++ Gymondo-Assestment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gymondo-Assestment
+//
+//  Created by Erfan Andesta on 3/21/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
