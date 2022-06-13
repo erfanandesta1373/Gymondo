68ec9ab4d979fad91994fa577ff1b4a2ffdb7805
Erfan Andesta <erfan.andesta@mindvalley.com>
2022-06-14 00:09:48 +0430

    [FEAT] Refactor exercise table view cell

--- Gymondo-Assestment/Modules/Exercises/Cells/ExerciseTableViewCell.swift
+++ Gymondo-Assestment/Modules/Exercises/Cells/ExerciseTableViewCell.swift
@@ -6,46 +6,70 @@
 //
 
 import UIKit
+import Combine
 
 class ExerciseTableViewCell: UITableViewCell, Reusable {
 
+    //MARK: - Properties -
     private let exerciseImageView = UIImageView()
-    private let name = UILabel()
+    private let nameLabel = UILabel()
+    private var cancellable: AnyCancellable?
     
+    //MARK: - Overrides -
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupView()
         self.selectionStyle = .none
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cancelImageLoading()
+    }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.selectionStyle = .none
         setupView()
     }
 
-}
+    //MARK: - Methods -
+    func bind(to exerciseViewModel: ExerciseViewModel) {
+        cancelImageLoading()
+        nameLabel.text = exerciseViewModel.name
+        cancellable = exerciseViewModel.image.sink(receiveValue: { [unowned self] image in
+            self.exerciseImageView.image = image
+        })
+    }
 
+    private func cancelImageLoading() {
+        exerciseImageView.image = nil
+        cancellable?.cancel()
+    }
+}
+//MARK: - ViewCode -
 extension ExerciseTableViewCell: ViewCode {
     func buildViewHierarchy() {
         addSubview(exerciseImageView)
-        addSubview(name)
+        addSubview(nameLabel)
     }
     
     func setupConstraints() {
-        exerciseImageView.pinLeft()
+        exerciseImageView.pinLeft(12)
         exerciseImageView.pinEdgesVerticallyToSuperview(10)
         exerciseImageView.width(with: 50, comparator: .equalToConstant)
         exerciseImageView.squareViewConstraint()
         
-        name.pinLeft(20, target: exerciseImageView)
-        name.pinRight()
-        name.centerVerticallyToSuperView()
+        nameLabel.pinLeft(20, target: exerciseImageView)
+        nameLabel.pinRight(12)
+        nameLabel.centerVerticallyToSuperView()
     }
     
     func setupAdditionalConfiguration() {
-        exerciseImageView.contentMode = .scaleAspectFill
+        exerciseImageView.contentMode = .scaleAspectFit
         exerciseImageView.layer.cornerRadius = 8
+        exerciseImageView.clipsToBounds = true
+        
+        nameLabel.numberOfLines = 0
     }
     
 }
